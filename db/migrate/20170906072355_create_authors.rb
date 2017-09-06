7ead437b86627e919d563e7312fb90cc3aad6ca7
Khanyiso-M <Khanyiso@PerfectCircle.co.za>
2017-09-06 09:46:38 +0200

    Testing branch commit

--- db/migrate/20170906072355_create_authors.rb
+++ db/migrate/20170906072355_create_authors.rb
@@ -3,6 +3,10 @@ class CreateAuthors < ActiveRecord::Migration[5.0]
     create_table :authors do |t|
 		t.string :name
 		t.text :bio
+		t.string :id_num
+		t.string :surname
+		t.text :lifestory
+		
       t.timestamps
     end
   end
